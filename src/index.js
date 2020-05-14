ab35e11b8a2ac65486e776285a37715dc680fb7d
Cristiano Augusto <cristiano.asg@hotmail.com>
2020-05-14 19:48:02 +0100

    Adicionando novos projetos

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import {
   Text,
   StyleSheet,
   StatusBar,
+  TouchableOpacity,
 } from "react-native";
 
 import api from "./services/api";
@@ -17,6 +18,16 @@ export default function App() {
       setProjects(response.data);
     });
   }, []);
+  async function handleAddProject() {
+    const response = await api.post("projects", {
+      title: `Novo projeto ${Date.now()}`,
+      owner: "Cristiano Augusto",
+    });
+
+    const project = response.data;
+
+    setProjects([...projects, project]);
+  }
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
@@ -29,6 +40,13 @@ export default function App() {
             <Text style={styles.project}>{project.title}</Text>
           )}
         />
+        <TouchableOpacity
+          activeOpacity={0.6}
+          style={styles.button}
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar projeto</Text>
+        </TouchableOpacity>
       </SafeAreaView>
     </>
   );
@@ -42,5 +60,17 @@ const styles = StyleSheet.create({
   project: {
     color: "#fff",
     fontSize: 30,
+  },
+  button: {
+    backgroundColor: "#FFF",
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
